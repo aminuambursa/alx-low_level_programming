0c421bc2018453fe43c88ff6307326ef4f8f9f5a
aminuambursa <aminumusaambursa123@gmail.com>
2023-09-24 07:26:11 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,15 @@
-#include <stdio.h>
-
+#include  <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - main function
+*
+*Return: always 0
+*/
+
 int main(void)
 {
-    char letter1 = 'a';
-    char letter2 = '\n';
-
-    putchar(letter1);
-
-    while (letter1 < 'z')
-    {
-        letter1++;
-        putchar(letter1);
-    }
-
-    putchar(letter2);
-
-    return (0);
+	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return(0);
 }
